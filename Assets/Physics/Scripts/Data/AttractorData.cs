65caa0965f4c26814678ec2af4a3c3f824307deb
jorgsiq <jorge.siqueira@yandex.com>
2020-04-30 21:18:44 +0100

    Version 0.1 / Large Files Fix

---
+++ Assets/Physics/Scripts/Data/AttractorData.cs
@@ -0,0 +1,23 @@
+﻿#region Copyright
+/// Copyright © 2017-2018 Vlad Kirpichenko
+/// 
+/// Author: Vlad Kirpichenko 'itanksp@gmail.com'
+/// Licensed under the MIT License.
+/// License: http://opensource.org/licenses/MIT
+#endregion
+
+using UnityEngine;
+
+namespace SimpleKeplerOrbits
+{
+	/// <summary>
+	/// Attractor data, necessary for calculation orbit.
+	/// </summary>
+	[System.Serializable]
+	public class AttractorData
+	{
+		public Transform AttractorObject;
+		public float AttractorMass = 1000;
+		public float GravityConstant = 0.1f;
+	}
+}
